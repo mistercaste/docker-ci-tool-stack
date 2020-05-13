41e8a94dff6bb1f64938cf39bb4763f810d97051
mistercaste <mistercaste@gmail.com>
2020-05-13 18:15:44 +0200

    Making use of forked GitHub repositories (90%)

--- jenkins/seedjobs/conferenceAppSeedJob.groovy
+++ jenkins/seedjobs/conferenceAppSeedJob.groovy
@@ -1,4 +1,4 @@
-def gitUrl = 'https://github.com/codecentric/conference-app'
+def gitUrl = 'https://github.com/mistercaste/conference-app'
 
 createCiJob("conference-app", gitUrl, "app/pom.xml")
 createSonarJob("conference-app", gitUrl, "app/pom.xml")
